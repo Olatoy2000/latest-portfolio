24e879a18a449d9339cf855eb0f1fb2887457187
oladejotoyyibolalekan <oladejotoyyibolalekan@gmail.com>
2023-05-16 14:45:37 +0100

    second commit

---
+++ pages/_app.tsx
@@ -0,0 +1,22 @@
+import { useRouter } from "next/router";
+import React, { ReactNode } from "react";
+import  "../styles/global.css"
+import { AppPropsX } from "../types/next";
+
+
+export default function App({ Component, pageProps }: AppPropsX) {
+  const GivenLayout = Component.Layout;
+  const DefaultLayout = ({ children }: { children: ReactNode }) => (
+    <>{children}</>
+  );
+  const Layout = GivenLayout || DefaultLayout;
+  const layoutProps = Component.LayoutProps || {};
+  const router = useRouter();
+
+  return (
+
+    <Layout {...layoutProps}>
+        <Component {...pageProps} />
+    </Layout>        
+  )
+}
