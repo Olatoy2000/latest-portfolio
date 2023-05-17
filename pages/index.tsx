052777233242dbd7a8b0999bdfda212100db94c0
oladejotoyyibolalekan <oladejotoyyibolalekan@gmail.com>
2023-05-17 15:36:37 +0100

    header

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,28 @@
+import Header from '@/components/Header'
+import Hero from '@/components/Hero'
+import { NextPage } from 'next'
+import Head from 'next/head'
 import React from 'react'
 
 const index = () => {
   return (
-    <div>this is the front page</div>
+    <div>
+      <Head> 
+        <title>OLATOY | Home</title>
+      </Head>
+        <Header />
+     {/* Hero */}
+        <Hero />
+     {/* About */}
+
+     {/* Experience */}
+
+     {/* Skills */}
+
+     {/* Projects */}
+
+     {/* Contadt Me */}
+    </div> 
   )
 }
 
---
+++ src/components/Header.tsx
@@ -0,0 +1,50 @@
+import React from 'react'
+import { SocialIcon } from "react-social-icons";
+import { motion } from "framer-motion"
+
+type Props = {}
+
+const Header = (props: Props) => {
+  return (
+    <header className='sticky top-0 flex  items-start xl:items-center z-20 p-5 justify-between mx-auto max-w-screen-2xl'>
+        <motion.div
+            initial={{
+                opacity: 0,
+                scale: 0.5,
+                x: -500
+            }}
+
+            animate={{
+                x:0,
+                opacity: 1,
+                scale: 1
+            }}
+            transition={{ duration: 1.5}}
+        >
+            <SocialIcon className='' url="https://www.youtube.com/@oladejotoyyibolalekan9129" 
+            fgColor='gray'
+            bgColor='transparent'
+            />
+            <SocialIcon url="https://www.youtube.com/@oladejotoyyibolalekan9129" 
+            fgColor='gray'
+            bgColor='transparent'
+            />
+            <SocialIcon url="https://www.youtube.com/@oladejotoyyibolalekan9129" 
+            fgColor='gray'
+            bgColor='transparent'
+            />
+        </motion.div>
+        <div className='flex cursor-pointer items-center text-gray-300'>
+            <SocialIcon
+            className='cursor-pointer'
+            network='email'
+            fgColor='gray'
+            bgColor='transparent'
+            />
+            <p className='uppercase hidden md:inline-flex text-sm text-gray-400'>Get In Touch</p>
+        </div>
+    </header>
+  )
+}
+
+export default Header
